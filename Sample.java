3cbd39207f5eb5bda5bc06200751bb29d1ace8eb
Shruthi1238 <61734780+Shruthi1238@users.noreply.github.com>
2020-03-05 15:23:31 +0530

    Created a Java File

---
+++ Sample.java
@@ -0,0 +1,13 @@
+public class Sample
+{
+  public static void main()
+  {
+   String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+	    
+	    for(int i=0;i<cars.length;i++)
+	    
+	    System.out.println(cars[i]);
+  }
+  
+
+}
